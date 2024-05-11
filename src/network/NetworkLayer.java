d7a689756789f4a91affda2fa442430212ee32cc
brahim <braboomtop@gmail.com>
2024-05-11 16:10:47 +0200

    update: ReLu function

--- src/network/NetworkLayer.java
+++ src/network/NetworkLayer.java
@@ -1,4 +1,4 @@
-package Network;
+package network;
 
 import java.util.Random;
 
@@ -8,7 +8,8 @@ public class NetworkLayer extends Layer {
     protected double[] errors;
     private final Layer prevLayer;
     private NetworkLayer nextLayer;
-    private double learningRate = 0.3;
+    private double learningRate = 0.8;
+    private final boolean RELU = false;
 
     public NetworkLayer(int layerSize, Layer prevLayer) {
         super(layerSize);
@@ -40,16 +41,15 @@ public class NetworkLayer extends Layer {
             for (int j = 0; j < weights[i].length; j++) {
                 values[i] += weights[i][j] * prevLayer.values[j];
             }
-            values[i] = ReLU(values[i] + biases[i]);
+            values[i] = activation(values[i] + biases[i]);
         }
     }
     public void calculateError(int label) {
         assert nextLayer == null;
         for (int i = 0; i < errors.length; i++) {
             int expected = label == i ? 1 : 0;
-            errors[i] = 2 * (expected - getValues()[i]) * ReLUDerivative(getValues()[i]);
+            errors[i] = 2 * (getValues()[i] - expected) * activationDerivative(getValues()[i]);
         }
-
     }
     public void calculateError() {
         assert nextLayer != null;
@@ -58,18 +58,25 @@ public class NetworkLayer extends Layer {
                 double errorFactor = nextLayer.errors[j] * nextLayer.weights[j][i];
                 errors[i] += errorFactor;
             }
-            errors[i] = ReLUDerivative(values[i]) * errors[i];
+            errors[i] = activationDerivative(values[i]) * errors[i];
         }
     }
     public void updateParams() {
         for (int i = 0; i < weights.length; i++) { //i current neurons
-            double factorBias = errors[i] *learningRate;
-            biases[i] += factorBias;
+            double factorBias = errors[i] * learningRate;
+            biases[i] -= factorBias;
             for (int j = 0; j < weights[i].length; j++) { // j previous neurons
                 double factorWeight = errors[i] * prevLayer.values[j] * learningRate;
-                weights[i][j] += factorWeight;
+                weights[i][j] -= factorWeight;
             }
         }
+    }
+    private double sigmoid(double x) {
+        double factor = 1 + Math.exp(-x);
+        return 1 / factor;
+    }
+    private double sigmoidDerivative(double x) {
+        return x * (1 - x);
     }
     private double ReLU(double x) {
         return (x > 0) ? x : 0;
@@ -77,13 +84,17 @@ public class NetworkLayer extends Layer {
     private double ReLUDerivative(double x) {
         return (x > 0) ? 1 : 0;
     }
-
-    private double sigmoid(double x) {
-        double factor = 1 + Math.exp(-x);
-        return 1 / factor;
+    private double activation(double x) {
+        if (RELU) {
+            return ReLU(x);
+        }
+        return sigmoid(x);
     }
-    private double sigmoidDerivative(double x) {
-        return x * (1 - x);
+    private double activationDerivative(double x) {
+        if (RELU) {
+            return ReLUDerivative(x);
+        }
+        return sigmoidDerivative(x);
     }
     public void decreaseLearningRate() {
         learningRate *= 0.9;
@@ -92,5 +103,4 @@ public class NetworkLayer extends Layer {
         values = new double[values.length];
         errors = new double[errors.length];
     }
-
 }
